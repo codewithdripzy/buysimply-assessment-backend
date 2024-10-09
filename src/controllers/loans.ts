da762cd2521068eef1cd75702e8c413892b25d44
Bankole Emmanuel <codewithdripzy@gmail.com>
2024-10-09 19:59:03 +0100

    feat: added filters

--- src/controllers/loans.ts
+++ src/controllers/loans.ts
@@ -6,6 +6,7 @@ import { HTTP_RESPONSE_CODE } from "../core/values";
 import Loans from "../model/loans";
 import { BuySimplyTokenStruct, LoanStruct } from "../core/struct";
 import { AccessLevel, LoanStatus } from "../core/enums";
+import { validateEmail } from "../utils/functions";
 
 const GetLoanController = async (req: Request, res: Response) => {
     try {
@@ -79,4 +80,84 @@ const GetLoanController = async (req: Request, res: Response) => {
     }
 }
 
-export { GetLoanController }
+const GetUserLoanController = async (req: Request, res: Response) => {
+    try {
+        configDotenv();
+        const { status } = req.query;
+        const { email } = req.params;
+        
+        const staff = new Staff();
+        const secret = process.env.BUYSIMPLY_JWT_SECRET;
+        const token = req.headers.authorization?.split(" ")[1];
+
+        if(!token) return res.status(HTTP_RESPONSE_CODE.UNAUTHORIZED).json({
+            error: "No Api Token Provided"
+        });
+
+        if(!secret) return res.status(HTTP_RESPONSE_CODE.INTERNAL_SERVER_ERROR).json({
+            error: " Something went wrong, Try again"
+        });
+
+        const loans = new Loans();
+        let loanData : LoanStruct[] | null = await loans.findAll();
+
+        if(!loanData) return res.status(HTTP_RESPONSE_CODE.OK).json({
+            data: []
+        });
+
+        const verifyToken : BuySimplyTokenStruct = jwt.verify(token, secret) as unknown as BuySimplyTokenStruct;
+        const userExists = await staff.findOne({
+            id: verifyToken.id,
+            email: verifyToken.email,
+            role: verifyToken.role as AccessLevel,
+        });
+       
+        if(!userExists) return res.status(HTTP_RESPONSE_CODE.UNAUTHORIZED).json({
+            error: "Account not Found, you are unable to perform this action"
+        });
+
+        
+        if(verifyToken.role == AccessLevel.STAFF){
+            loanData = loanData?.map((loan) => ({
+                id: loan.id,
+                amount: loan.amount,
+                maturityDate: loan.maturityDate,
+                status: loan.status,
+                applicant: {
+                    name: loan.applicant.name,
+                    email: loan.applicant.email,
+                    telephone: loan.applicant.telephone,
+                },
+                createdAt: loan.createdAt,
+            }));
+        }
+
+        if(status == LoanStatus.ACTIVE || status == LoanStatus.PENDING){
+            loanData = loanData.filter((loan) => loan.status == status);
+        }else{
+            return res.status(HTTP_RESPONSE_CODE.BAD_REQUEST).json({
+                error: `No status of Type<${status}> found`
+            })
+        }
+        
+        if(email){
+            if(!validateEmail(email)) return res.status(HTTP_RESPONSE_CODE.BAD_REQUEST).json({
+                message: "Invalid email provided"
+            });
+            loanData = loanData.filter((loan) => loan.applicant.email == email);
+        }
+
+        return res.status(HTTP_RESPONSE_CODE.OK).json(loanData)
+        // }else{
+        //     return res.status(HTTP_RESPONSE_CODE.INTERNAL_SERVER_ERROR).json({
+        //         message: "Something went wrong on our end, It's not your fault"
+        //     });
+        // }
+    } catch (error) {
+        return res.status(HTTP_RESPONSE_CODE.INTERNAL_SERVER_ERROR).json({
+            message: "An error ocurred, Try again"
+        });
+    }
+}
+
+export { GetUserLoanController }
--- src/routes/loans.ts
+++ src/routes/loans.ts
@@ -3,6 +3,7 @@ import { GetLoanController } from "../controllers/loans";
 
 const loanRouter = Router();
 
-loanRouter.route("/").get(GetLoanController)
+loanRouter.route("/").get(GetLoanController);
+loanRouter.route("/:email/get").get(GetUserLoanController);
 
 export default loanRouter;
